4d27bc1bd0b6915fd510cf9604767c075871cf16
awendle <awendle@users.noreply.github.com>
2018-03-31 17:02:50 -0500

    Product Manager Pages Built and Tested

---
+++ MyShopAwendle/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+
+            if(products == null)
+                products = new List<Product>();
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product p)
+        {
+            Product productToUpdate = products.Find(x => x.Id == p.Id);
+
+            if (productToUpdate != null)
+                productToUpdate = p;
+            else
+                throw new Exception("Product not found");
+        }
+
+        public Product Find(string id)
+        {
+            Product product = products.Find(x => x.Id == id);
+
+            if (product != null)
+                return product;
+            else
+                throw new Exception("Product not found");
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            Product productToDelete = products.Find(x => x.Id == id);
+
+            if (productToDelete != null)
+                products.Remove(productToDelete);
+        }
+    }
+}
